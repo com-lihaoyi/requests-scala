fdd634098f2caabcab2addf54204dee9710f6304
Lanqing Huang <lqhuang@outlook.com>
2026-04-20 21:55:20 +0800

    chore: Replace `testcontainers-scala` by `mccutchen/go-httpbin`

--- requests/test/src/requests/HttpbinTestSuite.scala
+++ requests/test/src/requests/HttpbinTestSuite.scala
@@ -1,21 +1,38 @@
 package requests
 
-import com.dimafeng.testcontainers.GenericContainer
-import org.testcontainers.containers.wait.strategy.Wait
-import utest._
+import java.util.concurrent.ThreadLocalRandom
+import java.net.BindException
+
+import utest.TestSuite
 
 abstract class HttpbinTestSuite extends TestSuite {
-  private val containerDef = GenericContainer.Def(
-    "kennethreitz/httpbin",
-    exposedPorts = Seq(80),
-    waitStrategy = Wait.forHttp("/"),
-  )
-  private val container = containerDef.start()
+  // `GO_HTTPBIN_EXE` is defined in build.mill to get the path to go-httpbin cli
+  val httpbinPath = sys.env("GO_HTTPBIN_EXE")
 
-  val localHttpbin: String =
-    s"${container.containerIpAddress}:${container.mappedPort(80)}"
+  private val host = "127.0.0.1"
+  private var port = ThreadLocalRandom.current().nextInt(10000, 65535).toString()
+
+  private val httpbinProcess =
+    try {
+      val process =
+        os.proc(httpbinPath, "-host", host, "--port", port, "-log-level", "ERROR").spawn()
+      if (process.waitFor(500L)) {
+        if (process.stderr.lines().mkString.contains("address already in use"))
+          throw new BindException("Port is already in use")
+        else
+          throw new IllegalThreadStateException("`httpbin` process failed to start")
+      }
+      process
+    } catch {
+      case exc: BindException => {
+        port = ThreadLocalRandom.current().nextInt(10000, 65535).toString()
+        os.proc(httpbinPath, "-host", "127.0.0.1", "--port", port).spawn()
+      }
+    }
+
+  val localHttpbin: String = s"${host}:${port}"
 
   override def utestAfterAll(): Unit = {
-    container.stop()
+    httpbinProcess.destroy()
   }
 }
